fa04d67ea910a5b6f78806c285a97d48ca42e361
DiegoSt23 <diego.stonerough@gmail.com>
2023-08-01 12:04:06 -0600

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'diego-react-delta-ui';
+
+const App = () => (
+  <Button>testing</Button>
+)
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'diego-react-delta-ui/dist/style.css'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
