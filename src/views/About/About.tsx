5442366a5cdfd22192303c663fc11d7433d0cca2
DiegoSt23 <diego.stonerough@gmail.com>
2023-09-01 18:35:49 -0600

    Add animations

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,50 +1,87 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import { Typography } from 'diego-react-delta-ui';
 import { ViewLayout } from '../../components';
 import styles from './about.module.scss';
 
-export const About = () => (
-  <ViewLayout id='about' title='About'>
-    <div className={styles.aboutMainContainer}>
-      <div className={styles.about}>
-        <div className={styles.textContainer}>
-          <Typography className={styles.text} type='paragraph'>
-            A Front-End Developer based in Mexico specialized in modern React.js
-            with TypeScript.
-          </Typography>
-          <br />
-          <Typography className={styles.text} type='paragraph'>
-            Equipped with an in-depth understanding of TypeScript, I embrace
-            type safety and modern development practices to ensure the highest
-            quality codebase. My expertise in front-end development extends to
-            crafting responsive layouts, beautiful interfaces and optimizing performance.
-          </Typography>
-          <br />
-          <Typography className={styles.text} type='paragraph'>
-            Driven by a strong problem-solving aptitude, a keen eye for detail
-            and a passion for crafting elegant and performant web and mobile
-            applications I take pride in delivering high-quality code that not
-            only meets project requirements but also adheres to best coding
-            practices and standards.
-          </Typography>
-        </div>
-        <div className={styles.textContainer}>
-          <Typography className={styles.text} type='paragraph'>
-            My commitment to writing clean, maintainable code, coupled with a
-            strong foundation in UI/UX principles and my passion for continuous
-            learning and innovation empowers me to embrace challenges and find
-            creative solutions to complex development tasks.
-          </Typography>
-          <br />
-          <Typography className={styles.text} type='paragraph'>
-            In my toolkit, React's component-based architecture is my go-to
-            canvas, allowing me to piece together intricate user interfaces that
-            seamlessly blend creativity with functionality. I thrive on
-            collaborating with designers, product managers, and fellow
-            developers to bring ideas to life in an agile and collaborative
-            environment.
-          </Typography>
+export const About = () => {
+  const { ref, inView } = useInView();
+  const [isInView, setIsInView] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (inView) {
+      setIsInView(true);
+    }
+  }, [inView]);
+
+  return (
+    <ViewLayout id='about' title='About'>
+      <div className={styles.aboutMainContainer}>
+        <div className={styles.about} ref={ref}>
+          {isInView && (
+            <>
+              <motion.div
+                className={styles.textContainer}
+                initial={{ opacity: 0, x: -60 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  delay: 0.2,
+                  duration: 0.7,
+                }}
+              >
+                <Typography className={styles.text} type='paragraph'>
+                  A Front-End Developer based in Mexico specialized in modern
+                  React.js with TypeScript.
+                </Typography>
+                <br />
+                <Typography className={styles.text} type='paragraph'>
+                  Equipped with an in-depth understanding of TypeScript, I
+                  embrace type safety and modern development practices to ensure
+                  the highest quality codebase. My expertise in front-end
+                  development extends to crafting responsive layouts, beautiful
+                  interfaces and optimizing performance.
+                </Typography>
+                <br />
+                <Typography className={styles.text} type='paragraph'>
+                  Driven by a strong problem-solving aptitude, a keen eye for
+                  detail and a passion for crafting elegant and performant web
+                  and mobile applications I take pride in delivering
+                  high-quality code that not only meets project requirements but
+                  also adheres to best coding practices and standards.
+                </Typography>
+              </motion.div>
+              <br />
+              <motion.div
+                className={styles.textContainer}
+                initial={{ opacity: 0, x: 60 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  delay: 0.2,
+                  duration: 0.7,
+                }}
+              >
+                <Typography className={styles.text} type='paragraph'>
+                  My commitment to writing clean, maintainable code, coupled
+                  with a strong foundation in UI/UX principles and my passion
+                  for continuous learning and innovation empowers me to embrace
+                  challenges and find creative solutions to complex development
+                  tasks.
+                </Typography>
+                <br />
+                <Typography className={styles.text} type='paragraph'>
+                  In my toolkit, React's component-based architecture is my
+                  go-to canvas, allowing me to piece together intricate user
+                  interfaces that seamlessly blend creativity with
+                  functionality. I thrive on collaborating with designers,
+                  product managers, and fellow developers to bring ideas to life
+                  in an agile and collaborative environment.
+                </Typography>
+              </motion.div>
+            </>
+          )}
         </div>
       </div>
-    </div>
-  </ViewLayout>
-);
+    </ViewLayout>
+  );
+};
--- src/views/Me/Me.tsx
+++ src/views/Me/Me.tsx
@@ -1,22 +1,57 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import { ViewLayout } from '../../components';
 import { Typography } from 'diego-react-delta-ui';
 import img from '../../assets/images/diego.jpg';
 import styles from './me.module.scss';
 
-export const Me = () => (
-  <ViewLayout id='me'>
-    <div className={styles.meMainContainer}>
-      <div className={styles.contentContainer}>
-        <div className={styles.contentSubContainer}>
-          <Typography type='heading1' className={styles.title}>
-            Hey, I'm Diego
-          </Typography>
-          <Typography type='heading3' className={styles.subtitle}>
-            frontEndDeveloper
-          </Typography>
+export const Me = () => {
+  const { ref, inView } = useInView();
+  const [isInView, setIsInView] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (inView) {
+      setIsInView(true);
+    }
+  }, [inView]);
+
+  return (
+    <ViewLayout id='me'>
+      <div className={styles.meMainContainer}>
+        <div className={styles.contentContainer} ref={ref}>
+          {isInView && (
+            <motion.div
+              className={styles.contentSubContainer}
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{
+                delay: 0.2,
+                duration: 0.7,
+              }}
+            >
+              <Typography type='heading1' className={styles.title}>
+                Hey, I'm Diego
+              </Typography>
+              <Typography type='heading3' className={styles.subtitle}>
+                frontEndDeveloper
+              </Typography>
+            </motion.div>
+          )}
+          {isInView && (
+            <motion.img
+              src={img}
+              alt='Diego'
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.2,
+                duration: 0.7,
+              }}
+            />
+          )}
         </div>
-        <img src={img} alt="Diego" />
       </div>
-    </div>
-  </ViewLayout>
-);
+    </ViewLayout>
+  );
+};
